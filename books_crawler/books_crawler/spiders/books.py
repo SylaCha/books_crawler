f6568d2bfe98e079d8b5ec1d02868705c28309d0
Syla <35025448+SylaCha@users.noreply.github.com>
2018-01-03 14:25:28 +0700

    Editing Source Code
    Editing Source Code

--- books_crawler/books_crawler/spiders/books.py
+++ books_crawler/books_crawler/spiders/books.py
@@ -4,16 +4,16 @@ from scrapy.http import Request
 
 class BooksSpider(Spider):
     name = 'books'
-    allowed_domains = ['books.toscrape.com/']
-    start_urls = ['http://books.toscrape.com/']
+    allowed_domains = ['books.toscrape.com']
+    start_urls = ['http://books.toscrape.com']
 
-    def parse_page(self, response):
+    def parse(self, response):
         books = response.xpath('//h3/a/@href').extract()
         for book in books:
             absolute_url = response.urljoin(book)
             yield Request(absolute_url, callback=self.parse_book)
 
-        # Next page
+        # Process next page
         next_page_url = response.xpath('//a[text()="next"]/@href').extract_first()
         absolute_next_page_url = response.urljoin(next_page_url)
         yield Request(absolute_next_page_url)
