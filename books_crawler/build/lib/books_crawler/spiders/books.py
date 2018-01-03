0a307e3e0f2898043870bedeedec9647be8e75a4
Syla <35025448+SylaCha@users.noreply.github.com>
2018-01-03 15:00:23 +0700

    Modified code and build (scrapinghub)
    Modified code and build (scrapinghub)

--- books_crawler/build/lib/books_crawler/spiders/books.py
+++ books_crawler/build/lib/books_crawler/spiders/books.py
@@ -1,15 +1,39 @@
 # -*- coding: utf-8 -*-
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
+from scrapy import Spider
+from scrapy.http import Request
 
-class BooksSpider(CrawlSpider):
+class BooksSpider(Spider):
     name = 'books'
-    allowed_domains = ['books.toscrape.com/']
-    start_urls = (
-        'http://books.toscrape.com/',
-    )
+    allowed_domains = ['books.toscrape.com']
+    start_urls = ['http://books.toscrape.com']
 
-    rules = (Rule(LinkExtractor(), callback='parse_page', follow=True),)
+    def parse(self, response):
+        books = response.xpath('//h3/a/@href').extract()
+        for book in books:
+            absolute_url = response.urljoin(book)
+            yield Request(absolute_url, callback=self.parse_book)
 
-    def parse_page(self, response):
-        pass
+        # Process next page
+        next_page_url = response.xpath('//a[text()="next"]/@href').extract_first()
+        absolute_next_page_url = response.urljoin(next_page_url)
+        yield Request(absolute_next_page_url)
+
+    def parse_book(self, response):
+        title = response.xpath('//h1/text()').extract_first()
+        price = response.xpath('//*[@class="price_color"]/text()').extract_first()
+
+        image_url = response.xpath('//img/@src').extract_first()
+        image_url = image_url.replace('../../', 'http://books.toscrape.com/')
+
+        rating = response.xpath('//*[contains(@class, "star-rating")]/@class').extract_first()
+        rating = rating.replace('star-rating', '')
+
+        description = response.xpath('//*[@id="product_description"]/following-sibling::p/text()').extract_first()
+
+        yield{
+            'title': title,
+            'price': price,
+            'image_url': image_url,
+            'rating': rating,
+            'description': description
+        }
